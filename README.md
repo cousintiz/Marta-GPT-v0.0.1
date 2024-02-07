03bf0d16f88dd5c915c75f7c30a9c05316cd818f
cousintiz <augustooalcides@gmail.com>
2024-02-07 20:38:16 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Marta-GPT-v0.0.1
-User Content Customized Generative Pre-training Transformer (ChatGPT API, LangChain, Streamlit, NLP, LLM)
+<h1 align="center">
+    Marta-GPT v0.0.1
+</h1>
+
+A basic personal assistant/chatbot that can instantly answer your questions and complete tasks within your documents, It was developed using Large Language Models (LLMs), LangChain, and Retrieval-Augmented Generation (RAG).
+
+## 💻Installation guide:
+#### Clone the repository
+```python
+git@github.com:cousintiz/Marta-GPT-v0.0.1.git
+```
+#### If you have python3.10 intalled run this shell script otherwise follow the steps below:
+```bash
+./setup.sh
+```
+#### Create a virtual environment for the project's dependencies:
+```python
+python3 -m venv <environment-name>
+```
+#### Ativacte the virtual environment:
+```python
+<environment-name>\Scripts\Activate.ps1
+```
+#### Install the packages on the requirements.txt:
+```python
+pip3 install -r requirements.txt
+```
+
+## Usage
+#### After all the steps above just run the App:
+```bash
+./app.sh
+```
+- Get an OpenAI API key [here](https://platform.openai.com/api-keys)
+- Submit an text file document with the relevant information to feed into the model once you run the app.
+
+## ⚒️Development Stack
+- [Langchain](https://github.com/hwchase17/langchain)
+- [Streamlit](https://streamlit.io/)
+- [GPT 3.5-Turbo](https://platform.openai.com/docs/models/gpt-3-5-turbo)
+
+## ⚙️Contribution
+You are allowed use any part of the software or ideas found on this repository for anything, feel free to contribute, with improvements, issues or whatever you find useful...
---
+++ main.py
@@ -0,0 +1,118 @@
+import streamlit as st
+from io import StringIO
+from langchain.chains import ConversationalRetrievalChain
+from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.memory import ConversationBufferMemory	
+
+gpt_model = "gpt-3.5-turbo-1106"
+
+# set local docs for langchain
+chat_history = None
+memory = None
+loader = None
+index = None 
+retriever = None
+llm = None
+api_key = None 
+
+# path to database
+infofile = "./database/data.txt"  
+
+# assistant prompt
+pre_prompt = "You are a friendly and helpful teaching assistant called Cousin. You explain concepts in great depth using simple terms."
+
+# titulo da pagina
+st.markdown("<h1 style='text-align: center; color: white;'>Marta-GPT v0.0.1</h1>", unsafe_allow_html=True)
+
+
+def setup_langchain():
+    global chat_history, memory, loader, index, llm, retriever, api_key 
+
+    chat_history = []
+    memory = ConversationBufferMemory(memory_key="chat_history",return_messages=True)
+
+    # set local docs for langchain
+    embeddings = OpenAIEmbeddings(api_key = api_key)
+    loader = DirectoryLoader("database/", glob= "**/*.txt")
+    index = VectorstoreIndexCreator(embedding = embeddings).from_loaders([loader])
+
+    #set up chain params:
+    llm = ChatOpenAI(model = gpt_model, api_key = api_key, temperature = 1, max_tokens = 128)
+    retriever = index.vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 2, "score_threshold": 1, "fetch_k": 16})
+
+
+def save_data(data: str) -> None:
+    """
+    Receives User data, to be embedded to the model
+    """
+    try:
+        with open(infofile, 'a') as file:
+            file.write(data)
+        print("Successfully saved data.")
+    except Exception as e:
+        print(f"Error {e} has occurred")
+
+
+def marta(question:str)  -> None:
+    #recieves prompt from user, and returns answer
+    chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever = retriever,
+        memory = memory,
+    )
+    result = chain.invoke({"question": question, "chat_history": chat_history})
+    chat_history.append((question, result['answer']))
+
+    return result['answer'].lower()
+
+
+# sidebar
+with st.sidebar:
+    
+    st.header("Provide a valid OpenAI API key🗝")
+    
+    while api_key is None:
+        api_key = st.sidebar.text_input("your key:", type = "password")
+
+    st.header("Provide data files with relevant info📄")
+    upload = st.file_uploader("Upload a txt file")
+    
+    if upload is not None:
+        stringio = StringIO(upload.getvalue().decode("utf-8"))
+        datafile = stringio.read()
+        save_data(datafile) # save data from file in path database
+        setup_langchain()
+        
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+prompt = st.chat_input()
+
+if prompt is not None:
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    # mandar a questao e receber resposta do langchain
+    answer = marta(prompt)
+
+    #mostrar resposta
+    with st.chat_message("assistant"):
+        st.write(answer)
+
+    message = {"role": "assistant", "content": answer}
+    st.session_state.messages.append(message)
+
+
